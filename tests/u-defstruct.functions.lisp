ca1242fc04952e5c111a25b96dff56d53e7e9121
Francois-Rene Rideau <fare@tunes.org>
2009-08-24 19:53:55 -0400

    Fix tests that broke when we moved from groveling with compile-file to groveling with load.

--- tests/u-defstruct.functions.lisp
+++ tests/u-defstruct.functions.lisp
@@ -1,5 +1,5 @@
 (cl:in-package :asdf-dependency-grovel-test)
 
-(eval-when (:compile-toplevel)
+(eval-when (:compile-toplevel :execute)
   (defvar *function-defstruct-2* (make-test-defstruct/function :a 2 :b 3 :c 4))
   (test-defstruct/function-a *function-defstruct-2*))
